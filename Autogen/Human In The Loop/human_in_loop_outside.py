d7c194b9ee8d2c658c5cd52a12076c776b4f5380
Aditya-Chidre <chidreaditya141@gmail.com>
2025-08-23 14:22:10 +0530

    agent teams added

---
+++ Autogen/Human In The Loop/human_in_loop_outside.py
@@ -0,0 +1,60 @@
+import asyncio
+from codecs import StreamReader
+from autogen_agentchat.agents import AssistantAgent
+
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_agentchat.messages import TextMessage
+from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_agentchat.conditions import TextMentionTermination
+from dotenv import load_dotenv
+from autogen_agentchat.ui import Console
+import os
+
+load_dotenv()
+api_key = os.getenv('GOOGLE_API_KEY')
+model_client = OpenAIChatCompletionClient(
+    model='gemini-2.5-flash',
+    api_key=api_key
+)
+
+assistant1 = AssistantAgent(
+    name='Writer',
+    description='you are a great writer',
+    model_client=model_client,
+    system_message='You are a really helpful writer who writes in less than 30 words.'
+)
+
+assistant2 = AssistantAgent(
+    name='Reviewer',
+    description='you are a great reviewer',
+    model_client=model_client,
+    system_message='You are a really helpful reviewer who writes in less than 30 words..'
+)
+
+assistant3 = AssistantAgent(
+    name='Editor',
+    description='you are a great editor',
+    model_client=model_client,
+    system_message='You are a really helpful editor who writes in less than 30 words..'
+)
+
+team = RoundRobinGroupChat(
+    participants=[assistant1, assistant2, assistant3],
+    max_turns=3
+)
+
+async def main():
+    task = "Write a poem about india"
+    while True:
+        stream = team.run_stream(task=task)
+        await Console(stream)
+        
+        feedback = input("Enter the feedback to an agent: ")
+        
+        if feedback.lower().strip() == 'exit':
+            break
+        
+        task = feedback
+        
+if __name__ == '__main__':
+    asyncio.run(main())
